3023aa3e4da0de91d1cd592424c19cde3b45cdd2
enjoy.liu <enjoy.liu@huolala.cn>
2020-10-26 00:48:48 +0800

    UPDATE

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -7,7 +7,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
         .card-body {
-            padding: 1.25rem 0.5rem;
+            padding: 0;
+        }
+        .btn {
+            margin: auto 3px;
         }
     </style>
 </head>
@@ -25,8 +28,8 @@
         <div class="card mb-3 h-100" style="max-width: 18rem;">
           <div class="card-header">{{$item['name']}}</div>
           <div class="card-body text-dark">
-            <img src="{{$item['url']}}" style="width: 100%;margin: 10px auto">
-            <a href="/show/{{$item['id']}}" class="btn btn-info btn-sm" role="button">看大图</a>
+            <img src="{{$item['url']}}" style="width: 100%;margin: 0 0 10px 0">
+            <a href="/show/{{$item['id']}}" class="btn btn-info btn-sm" role="button">展示</a>
             <a href="/link/edit/{{$item['id']}}" class="btn btn-success btn-sm" role="button">编辑</a>
             <a href="/link/delete/{{$item['id']}}" class="btn btn-secondary btn-sm" role="button">删除</a>
           </div>
