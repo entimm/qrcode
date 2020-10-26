af4bb12cd600febd359c691c368de8569b9ea300
enjoy.liu <enjoy.liu@huolala.cn>
2020-10-26 09:34:45 +0800

    UPDATE

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -28,7 +28,7 @@
         <div class="card mb-3 h-100" style="max-width: 18rem;">
           <div class="card-header">{{$item['name']}}</div>
           <div class="card-body text-dark">
-            <img src="{{$item['url']}}" style="width: 100%;margin: 0 0 10px 0">
+            <a href="/show/{{$item['id']}}"><img src="{{$item['url']}}" style="width: 100%;margin: 0 0 10px 0"></a>
             <a href="/show/{{$item['id']}}" class="btn btn-info btn-sm" role="button">展示</a>
             <a href="/link/edit/{{$item['id']}}" class="btn btn-success btn-sm" role="button">编辑</a>
             <a href="/link/delete/{{$item['id']}}" class="btn btn-secondary btn-sm" role="button">删除</a>
