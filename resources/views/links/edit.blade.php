f5c8f1ff1a5d6c793932f279ad183030c81e5ed5
enjoy.liu <enjoy.liu@huolala.cn>
2020-11-29 13:27:18 +0800

    UPDATE

--- resources/views/links/edit.blade.php
+++ resources/views/links/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>二维码更新</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
