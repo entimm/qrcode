26e9f90f53871e5649c2b5f88f6a3c8ac3260be6
enjoy.liu <enjoy.liu@huolala.cn>
2020-11-29 22:24:54 +0800

    UPDATE

--- resources/views/links/show.blade.php
+++ resources/views/links/show.blade.php
@@ -19,7 +19,7 @@
       .jumbotron .container h1 {
           text-align: center;
           color: #007bff;
-          font-size: 1.6rem;
+          font-size: 1.3rem;
           /*font-weight: 200;*/
       }
       .jumbotron {
